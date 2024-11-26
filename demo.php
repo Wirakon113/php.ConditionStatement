e40bf8c1980e86b5825123041734899fd63436a0
Wirakon113 <oem555nmk@gmail.com>
2024-11-26 10:30:03 +0700

    update

---
+++ demo.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>ทดสอบส่งค่าเป็น method GET</h2>
+    <a href="test.php?subject=PHP&web=www.google.com">Click</a>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ทดสอบส่งค่าโดยใช้ Mehtod GET</title>
+</head>
+<body>
+    <h1>ทดสอบส่งค่าโดยใช้ Mehtod GET</h1>
+    <?php 
+    echo "ศึกษาเรื่อง".$_GET['subject']."ได้ที่".$_GET['web'];
+    ?>
+</body>
+</html> 
